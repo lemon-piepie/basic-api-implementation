c671d62bde36f5f3da1567826c546b5b894e11bc
lemon-piepie <purplechilde@163.com>
2020-08-04 14:28:56 +0800

    finished routing branch.

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,29 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class RsListApplicationTests {
-
+    @Autowired
+    MockMvc mockMvc;
     @Test
     void contextLoads() {
+        try {
+            mockMvc.perform(get("rs/list"))
+                    .andExpect(content().string("[第一条事件,第二条事件,第三条事件]"))
+                    .andExpect(status().isOk())
+                    .andReturn();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
