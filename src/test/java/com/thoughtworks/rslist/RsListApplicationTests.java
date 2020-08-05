b0e4227d2b61bf935d993377d45a2fa66ca027f6
lemon-piepie <purplechilde@163.com>
2020-08-05 13:15:08 +0800

    gender should not empty

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -138,4 +138,16 @@ class RsListApplicationTests {
         mockMvc.perform(post("/rs/event").content(postUserEventJson).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    void genderShouldNotEmpty() throws Exception {
+        UserDetiles userNew = new UserDetiles(
+                "Ming",20,null,"xiaowang@qq.com","18912341234");
+        RsEvent postUserEvent = new RsEvent("PS5发布会","游戏",userNew);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String postUserEventJson = objectMapper.writeValueAsString(postUserEvent);
+
+        mockMvc.perform(post("/rs/event").content(postUserEventJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 }
