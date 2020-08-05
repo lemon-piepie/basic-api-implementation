a17f6ff6d7d16adadf67a913e03b62c24c02154b
lemon-piepie <purplechilde@163.com>
2020-08-05 17:35:11 +0800

    do not get user filed when get full list

---
+++ src/main/java/com/thoughtworks/rslist/dominate/RsEvent.java
@@ -0,0 +1,49 @@
+package com.thoughtworks.rslist.dominate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.Valid;
+
+public class RsEvent {
+    private String eventName;
+    private String keyWord;
+
+    @Valid
+    private UserDetiles user;
+
+    public  RsEvent(String eventName, String keyWord,UserDetiles user){
+        this.eventName = eventName;
+        this.keyWord = keyWord;
+        this.user = user;
+    }
+
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getKeyWord() {
+        return keyWord;
+    }
+
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
+
+    @JsonIgnore
+    public UserDetiles getUser() {
+        return user;
+    }
+
+    @JsonProperty
+    public void setUser(UserDetiles user) {
+        this.user = user;
+    }
+
+}
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,84 @@
 package com.thoughtworks.rslist;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.dominate.RsEvent;
+import com.thoughtworks.rslist.dominate.UserDetiles;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void shouldGetRsList() throws Exception {
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$[0].eventName",is("理财产品")))
+                .andExpect(jsonPath("$[1].eventName",is("最新款5G手机")))
+                .andExpect(jsonPath("$[2].eventName",is("猪肉涨价了")))
+                .andExpect(jsonPath("$[0].keyWord",is("金融")))
+                .andExpect(jsonPath("$[1].keyWord",is("科技")))
+                .andExpect(jsonPath("$[2].keyWord",is("生活")))
+                .andExpect(jsonPath("$[0]",not(hasKey("user"))))
+                .andExpect(jsonPath("$[1]",not(hasKey("user"))))
+                .andExpect(jsonPath("$[2]",not(hasKey("user"))))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void shouldGetOneList() throws Exception {
+        mockMvc.perform(get("/rs/2"))
+                .andExpect(jsonPath("$.eventName",is("最新款5G手机")))
+                .andExpect(jsonPath("$.keyWord",is("科技")))
+                .andExpect(jsonPath("$.user.userName",is("Hua")))
+                .andExpect(jsonPath("$.user.age",is(20)))
+                .andExpect(jsonPath("$.user.gender",is("female")))
+                .andExpect(jsonPath("$.user.email",is("xiaohua@163.com")))
+                .andExpect(jsonPath("$.user.phoneNumber",is("11223344556")))
+                .andExpect(status().isOk());
+    }
+
+
+
+    @Test
+    void shouldAddRsEventToList() throws Exception {
+        UserDetiles userNew = new UserDetiles(
+                "Wang",22,"male","xiaowang@qq.com","18912341234");
+        RsEvent postUserEvent = new RsEvent("PS5发布会","游戏",userNew);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String postUserEventJson = objectMapper.writeValueAsString(postUserEvent);
+
+        mockMvc.perform(post("/rs/event").content(postUserEventJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$[0].eventName",is("理财产品")))
+                .andExpect(jsonPath("$[1].eventName",is("最新款5G手机")))
+                .andExpect(jsonPath("$[2].eventName",is("猪肉涨价了")))
+                .andExpect(jsonPath("$[3].eventName",is("PS5发布会")))
+                .andExpect(jsonPath("$[0].keyWord",is("金融")))
+                .andExpect(jsonPath("$[1].keyWord",is("科技")))
+                .andExpect(jsonPath("$[2].keyWord",is("生活")))
+                .andExpect(jsonPath("$[3].keyWord",is("游戏")))
+                .andExpect(jsonPath("$[3].user.userName",is("Wang")))
+                .andExpect(jsonPath("$[3].user.age",is(22)))
+                .andExpect(jsonPath("$[3].user.gender",is("male")))
+                .andExpect(jsonPath("$[3].user.email",is("xiaowang@qq.com")))
+                .andExpect(jsonPath("$[3].user.phoneNumber",is("18912341234")))
+                .andExpect(status().isOk());
     }
 
 }
